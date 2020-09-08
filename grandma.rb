f5f21f7b5d85efe91a8f908188fa26ae2e361dcb
Leah Schlackman <leahemily1@gmail.com>
2020-09-08 23:55:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if #{"phrase"} == #{"phrase".downcase}
+  if phrase == phrase.downcase
   return "HUH!? SPEAK UP SONNY!"
-  elsif #{"phrase"} == #{"phrase".upcase}
+  elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
-  elsif #{"phrase"} == "I love you Grandma!"
+  elsif phrase == "I love you Grandma!"
   return "I LOVE YOU TOO PUMPKIN!"
 end
 end
